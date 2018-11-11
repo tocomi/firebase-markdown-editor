52157935effece76d75028028a28213843348313
tocomi <tocomi0112@gmail.com>
2018-11-11 16:33:01 +0900

    add markdown editor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,24 +1,47 @@
 <template>
   <div id="editor">
     <h1>Markdown Editor</h1>
-    <h3>{{ user.displayName }}</h3>
+    <span>{{ user.displayName }}</span>
     <button @click="logout">Logout</button>
+    <div class="editorWrapper">
+      <textarea class="markdown" v-model="markdown"></textarea>
+      <div class="preview" v-html="preview()"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import marked from "marked"
 export default {
   name: "editor",
   props: [ "user" ],
   data() {
     return {
-      
+      markdown: "",
     }
   },
   methods: {
     logout() {
       firebase.auth().signOut()
+    },
+    preview() {
+      return marked(this.markdown)
     }
   }
 }
 </script>
+
+<style scoped>
+.editorWrapper {
+  display: flex;
+}
+.markdown {
+  width: 50%;
+  height: 500px;
+}
+.preview {
+  width: 50%;
+  text-align: left;
+}
+</style>
+
