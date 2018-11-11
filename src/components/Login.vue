c3c22f94940be0e9383e9a6b2f605378d049266c
tocomi <tocomi0112@gmail.com>
2018-11-11 15:00:09 +0900

    add login view

---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="login">
+    <button>Login Google Account</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
