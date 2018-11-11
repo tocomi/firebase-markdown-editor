5a7bb638973a1fa76c3f437d1ba5d375b1bab268
tocomi <tocomi0112@gmail.com>
2018-11-11 15:06:40 +0900

    add editor view

---
+++ src/components/Editor.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="editor">
+    <h1>Markdown Editor</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "editor",
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
